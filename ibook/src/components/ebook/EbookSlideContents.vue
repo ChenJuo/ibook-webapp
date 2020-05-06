bae8d7a67bfae7a0ea4b6bf01f257d39e0b24272
chenzhuo <jingxiaofeng1023@gmail.com>
2020-05-06 15:14:28 +0800

    全文搜索功能实现

--- ibook/src/components/ebook/EbookSlideContents.vue
+++ ibook/src/components/ebook/EbookSlideContents.vue
@@ -1,19 +1,24 @@
+<!--suppress ALL -->
 <template>
     <div class="ebook-slide-contents">
-       <div class="slide-contents-search-wrapper">
-           <div class="slide-contents-search-input-wrapper">
-               <div class="slide-contents-search-icon">
-                   <span class="icon-search"></span>
+        <div class="slide-contents-search-wrapper">
+            <div class="slide-contents-search-input-wrapper">
+                <div class="slide-contents-search-icon">
+                    <span class="icon-search"></span>
 
-               </div>
-               <input class="slide-contents-search-input"
-                      type="text"
-                      @click="showSearchPage()"
-                      :placeholder="$t('book.searchHint')"/>
-           </div>
-           <div class="slide-contents-search-cancel" v-if="searchVisible" @click="hideSearchPage()">{{$t('book.cancel')}}</div>
-       </div>
-        <div class="slide-contents-book-wrapper">
+                </div>
+                <input class="slide-contents-search-input"
+                       type="text"
+                       v-model="searchText"
+                       @click="showSearchPage()"
+                       @keyup.enter.exact="search()"
+                       :placeholder="$t('book.searchHint')"/>
+            </div>
+            <div class="slide-contents-search-cancel" v-if="searchVisible" @click="hideSearchPage()">
+                {{$t('book.cancel')}}
+            </div>
+        </div>
+        <div class="slide-contents-book-wrapper" v-show="!searchVisible">
             <div class="slide-contents-book-img-wrapper">
                 <img :src="cover" class="slide-contents-book-img"/>
             </div>
@@ -32,11 +37,25 @@
         <scroll class="slide-contents-list"
                 :top="156"
                 :bottom="48"
+                v-show="!searchVisible"
                 ref="scroll">
             <div class="slide-contents-item" v-for="(item,index) in navigation" :key="index">
-                <span class="slide-contents-item-label" :class="{'selected':section === index}" :style="contentItemStyle(item)" @click="displayNavigation(item.href)">{{item.label}}</span>
+                <span class="slide-contents-item-label" :class="{'selected':section === index}"
+                      :style="contentItemStyle(item)" @click="displayContent(item.href)">{{item.label}}</span>
                 <span class="slide-contents-item-page"></span>
             </div>
+        </scroll>
+        <scroll class="slide-search-list"
+                :top="66"
+                :bottom="48"
+                v-show="searchVisible">
+            <div class="slide-search-item"
+                 v-for="(item,index) in searchList"
+                 :key="index"
+                 v-html="item.excerpt"
+                 @click="displayContent(item.cfi,true)">
+            </div>
+
         </scroll>
     </div>
 </template>
@@ -44,141 +63,186 @@
 <script>
     import {ebookMixin} from "../../utils/mixin";
     import Scroll from '../common/Scroll';
-    import { px2rem } from "../../utils/utils";
+    import {px2rem} from "../../utils/utils";
 
     export default {
         name: "EbookSlideContents",
-        mixins:[ebookMixin],
-        components:{
-          Scroll
+        mixins: [ebookMixin],
+        components: {
+            Scroll
         },
-        data(){
-            return{
-                searchVisible:false
+        data() {
+            return {
+                searchText:'',
+                searchVisible: false,
+                searchList:[]
             }
         },
-        methods:{
-            displayNavigation(target){
-                this.display(target,() =>{
+        methods: {
+            search(){
+                if(this.searchText && this.searchText.length > 0){
+                    this.doSearch(this.searchText).then(list =>{
+                        this.searchList = list
+                        this.searchList.map(item =>{
+                            item.excerpt = item.excerpt.replace(this.searchText,`<span class="content-search-text">${this.searchText}</span>`)
+                            return item
+                        })
+                        })
+                    }
+            },
+            doSearch(q) {
+                return Promise.all(
+                    this.currentBook.spine.spineItems.map(
+                        section => section.load(this.currentBook.load.bind(this.currentBook))
+                            .then(section.find.bind(section, q))
+                            .finally(section.unload.bind(section)))
+                ).then(results => Promise.resolve([].concat.apply([], results)));
+            }
+            ,
+            displayContent(target,highlight = false) {
+                this.display(target, () => {
                     this.hideTitleAndMenu()
+                    if(highlight){
+                        this.currentBook.rendition.annotations.highlight(target)
+                    }
                 })
-            },
-            contentItemStyle(item){
-                return{
-                    marginLeft:`${px2rem(item.level * 15)} rem`
-                }
-            },
-            showSearchPage(){
-                this.searchVisible = true
-            },
-            hideSearchPage(){
-                this.searchVisible = false
             }
+            ,
+            contentItemStyle(item) {
+                return {
+                    marginLeft: `${px2rem(item.level * 15)} rem`
+                }
+            }
+            ,
+            showSearchPage() {
+                this.searchVisible = true
+            }
+            ,
+            hideSearchPage() {
+                this.searchVisible = false
+                this.searchText = ''
+                this.searchList = null
+            }
+        },
+        mounted(){
+
         }
     }
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
     @import "../../assets/styles/global";
-    .ebook-slide-contents{
+
+    .ebook-slide-contents {
         width: 100%;
         font-size: 0;
-        .slide-contents-search-wrapper{
+        .slide-contents-search-wrapper {
             display: flex;
             width: 100%;
             height: px2rem(36);
             margin: px2rem(20) 0 px2rem(10) 0;
             padding: 0 px2rem(15);
             box-sizing: border-box;
-            .slide-contents-search-input-wrapper{
+            .slide-contents-search-input-wrapper {
                 flex: 1;
                 @include center;
-                .slide-contents-search-icon{
+                .slide-contents-search-icon {
                     flex: 0 0 px2rem(28);
                     font-size: px2rem(12);
                     @include center;
                 }
-                .slide-contents-search-input{
+                .slide-contents-search-input {
                     flex: 1;
                     width: 100%;
                     height: px2rem(32);
                     font-size: px2rem(14);
                     background: transparent;
                     border: none;
-                    &:focus{
+                    &:focus {
                         outline: none;
                     }
                 }
             }
-            .slide-contents-search-cancel{
+            .slide-contents-search-cancel {
                 flex: 0 0 px2rem(50);
                 font-size: px2rem(14);
                 @include right;
             }
         }
-        .slide-contents-book-wrapper{
+        .slide-contents-book-wrapper {
             display: flex;
             width: 100%;
             height: px2rem(90);
             padding: px2rem(10) px2rem(15) px2rem(20) px2rem(15);
             box-sizing: border-box;
-            .slide-contents-book-img-wrapper{
+            .slide-contents-book-img-wrapper {
                 flex: 0 0 px2rem(45);
-                .slide-contents-book-img{
+                .slide-contents-book-img {
                     width: px2rem(45);
                     height: px2rem(60);
                 }
             }
-            .slide-contents-book-info-wrapper{
+            .slide-contents-book-info-wrapper {
                 flex: 1;
                 padding: 0 px2rem(10);
                 box-sizing: border-box;
-                .slide-contents-book-title{
+                .slide-contents-book-title {
                     width: px2rem(153.75);
                     font-size: px2rem(14);
                     line-height: px2rem(16);
                     @include ellipsis2(3);
                 }
-                .slide-contents-book-author{
+                .slide-contents-book-author {
                     width: px2rem(153.75);
                     font-size: px2rem(12);
                     margin-top: px2rem(5);
                     @include ellipsis;
                 }
             }
-            .slide-contents-book-progress-wrapper{
+            .slide-contents-book-progress-wrapper {
                 flex: 0 0 px2rem(70);
-                .slide-contents-book-progress{
+                .slide-contents-book-progress {
                     padding-top: px2rem(15);
-                    .progress{
+                    .progress {
                         font-size: px2rem(14);
                     }
-                    .progress-text{
+                    .progress-text {
                         font-size: px2rem(12);
                     }
                 }
-                .slide-contents-book-time{
+                .slide-contents-book-time {
                     font-size: px2rem(12);
                     margin-top: px2rem(5);
                 }
             }
         }
-        .slide-contents-list{
+        .slide-contents-list {
             padding: 0 px2rem(15);
             box-sizing: border-box;
-            .slide-contents-item{
+            .slide-contents-item {
                 display: flex;
                 padding: px2rem(20) 0;
                 box-sizing: border-box;
-                .slide-contents-item-label{
+                .slide-contents-item-label {
                     flex: 1;
                     font-size: px2rem(14);
                     line-height: px2rem(16);
                     @include ellipsis;
                 }
-                .slide-contents-item-page{
+                .slide-contents-item-page {
 
                 }
+            }
+        }
+        .slide-search-list{
+            padding: 0 px2rem(15);
+            box-sizing: border-box;
+            .slide-search-item{
+                width: 100%;
+                font-size: px2rem(14);
+                line-height: px2rem(16);
+                padding: px2rem(20) 0;
+                box-sizing: border-box;
             }
         }
     }
